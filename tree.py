7d594f8dae1cfe4355769442f83dc11b99e82604
MLGNicks <nicholassoetikno@gmail.com>
2022-12-21 09:58:53 +0700

    Edit tree, resize png

--- tree.py
+++ tree.py
@@ -118,28 +118,28 @@ dialogueTree.insert(130,text,choice,nextVal, asset)
 
 #Binary Tree 66
 asset = ['Background/hutan siang.jpg', 'Character/prince2.png', 'ds-icon.png']
-text = ['Apa ini?', 'Halo Halo Loha, aku DS, yang akan menemani mu di perjalanan ini.', 'Kamu kenapa bisa ada disini?', 'Aku diciptakan untuk membantu orang” yang tersesat di hutan ini.', 'Baiklah.']
+text = ['Apa ini?', 'Halo Halo Loha, aku DS, yang akan menemani mu di perjalanan ini.', 'Kamu kenapa bisa ada disini?', 'Aku diciptakan untuk membantu orang” yang tersesat di hutan ini.', 'Baiklah.', 'Struktur data yang menyimpan elemen dengan konsep fifo']
 choice = ['Queue', 'Stack']
 nextVal = [34,  98]
 dialogueTree.insert(66,text,choice,nextVal, asset)
 
 #Binary Tree 34
 asset = ['Background/hutan siang.jpg', 'Character/prince2.png', 'ds-icon.png']
-text = ['Lihat… aku berhasil mendapatkan 1 digit.', 'Selamat, kamu hanya perlu mencari 1 digit lagi.', 'Aku tidak sabar untuk segera pulang', 'mari kita lanjutkan perjalanan ini.']
-choice = ['Queue', 'Stack']
+text = ['Lihat… aku berhasil mendapatkan 1 digit.', 'Selamat, kamu hanya perlu mencari 1 digit lagi.', 'Aku tidak sabar untuk segera pulang', 'mari kita lanjutkan perjalanan ini.', 'Integer merupakan tipe data apa?']
+choice = ['Tunggal', ' Majemuk']
 nextVal = [18,  50]
 dialogueTree.insert(34,text,choice,nextVal, asset)
 
 #Binary Tree 18
 asset = ['Background/dalam rumah kosong.png', 'Character/prince2.png', 'Character/villain1.png', 'Character/villain2.png']
-text = ['Ayo cari pangeran itu, dia pasti tidak jauh dari sini.', 'Kita sudah mencari semalaman dan tidak menemukannya.', 'Apa mungkin dia sudah berhasil kembali ke kerajaannya?', 'Tidak mungkin! Cepat cari lagi, kita harus menemukannya.', 'Itu adalah orang-orang yang menculikku! Aku harus kabur dari sini.', 'Aku harus menyeberangi sungai ini.', 'Bagaimana caraku untuk menyeberang?']
+text = ['Ayo cari pangeran itu, dia pasti tidak jauh dari sini.', 'Kita sudah mencari semalaman dan tidak menemukannya.', 'Apa mungkin dia sudah berhasil kembali ke kerajaannya?', 'Tidak mungkin! Cepat cari lagi, kita harus menemukannya.', 'Itu adalah orang-orang yang menculikku! Aku harus kabur dari sini.', 'Aku harus menyeberangi sungai ini.', 'Bagaimana caraku untuk menyeberang?', ' Node tidak memiliki parent node, hanya memiliki children disebut?']
 choice = ['Root', 'Leaf']
 nextVal = [10,  26]
 dialogueTree.insert(18,text,choice,nextVal, asset)
 
 #Binary Tree 10
 asset = ['Background/sungai.jpg', 'Others/row boat.png']
-text = ['']
+text = ['Jenis graph yang dapat menjelajah 2 arah adalah    ']
 choice = ['Undirected', 'Directed']
 nextVal = [6,  14]
 dialogueTree.insert(10,text,choice,nextVal, asset)
@@ -188,14 +188,14 @@ dialogueTree.insert(16,text,choice,nextVal, asset)
 
 #Binary Tree 26
 asset = ['Background/sungai.jpg', 'Character/prince2.png']
-text = ['Air sungai ini sangat dingin! Tetapi sepertinya aku tidak punya pilihan lain selain berenang.']
+text = ['Air sungai ini sangat dingin! Tetapi sepertinya aku tidak punya pilihan lain selain berenang.', 'Queue merupakan struktur data apa?']
 choice = ['Majemuk', 'Sederhana']
 nextVal = [22,  30]
 dialogueTree.insert(26,text,choice,nextVal, asset)
 
 #Binary Tree 22
 asset = ['Background/sungai.jpg', 'Character/prince2.png']
-text = ['Tidak sia-sia aku berenang ke sini, ternyata aku mendapatkan digit terakhir yang kuperlukan! Tetapi aku sangat kedinginan.', 'Ah, aku akan menghangatkan diri sebentar dengan menyalakan api unggun.', ]
+text = ['Tidak sia-sia aku berenang ke sini, ternyata aku mendapatkan digit terakhir yang kuperlukan! Tetapi aku sangat kedinginan.', 'Ah, aku akan menghangatkan diri sebentar dengan menyalakan api unggun.', ' "by boundary" dan "by shifting" merupakan jenis dari']
 choice = ['folding', 'carry']
 nextVal = [20,  24]
 dialogueTree.insert(22,text,choice,nextVal, asset)
@@ -216,7 +216,7 @@ dialogueTree.insert(24,text,choice,nextVal, asset)
 
 #Binary Tree 30
 asset = ['Background/sungai.jpg', 'Character/prince2.png']
-text = ['Tidak sia-sia aku berenang ke sini, ternyata aku mendapatkan digit terakhir yang kuperlukan! Tetapi aku sangat kedinginan..', 'Ah, aku akan menghangatkan diri sebentar dengan menyalakan api unggun.']
+text = ['Tidak sia-sia aku berenang ke sini, ternyata aku mendapatkan digit terakhir yang kuperlukan! Tetapi aku sangat kedinginan..', 'Ah, aku akan menghangatkan diri sebentar dengan menyalakan api unggun.', 'Salah satu metode transversal tree']
 choice = ['Inorder', 'Postorder']
 nextVal = [28,32]
 dialogueTree.insert(30,text,choice,nextVal, asset)
@@ -237,28 +237,28 @@ dialogueTree.insert(32,text,choice,nextVal, asset)
 
 #Binary Tree 50
 asset = ['Background/rumah kosong.jpg', 'Character/prince2.png', 'Character/villain1.png', 'Character/villain2.png']
-text = ['Ayo cari pangeran itu, dia pasti tidak jauh dari sini.', 'Kita sudah mencari semalaman dan tidak menemukannya.', 'Apa mungkin dia sudah berhasil kembali ke kerajaannya?', 'Tidak mungkin! Cepat cari lagi, kita harus menemukannya.', 'Itu adalah orang-orang yang menculikku! Aku harus kabur dari sini.']
+text = ['Ayo cari pangeran itu, dia pasti tidak jauh dari sini.', 'Kita sudah mencari semalaman dan tidak menemukannya.', 'Apa mungkin dia sudah berhasil kembali ke kerajaannya?', 'Tidak mungkin! Cepat cari lagi, kita harus menemukannya.', 'Itu adalah orang-orang yang menculikku! Aku harus kabur dari sini.', 'Heap dimana root-nya memiliki value paling kecil adalah?']
 choice = ['Min', 'Max']
 nextVal = [42,  58]
 dialogueTree.insert(50,text,choice,nextVal, asset)
 
 #Binary Tree 58
 asset = ['Background/black panther.jpg','Character/prince2.png']
-text = ['']
+text = ['Linked List yang semua pointernya menunjuk pada', 'sebuah node atau tidak ada yang bernilai NULL adalah?']
 choice = ['Circular', 'Double']
 nextVal = [54, 62]
 dialogueTree.insert(58,text,choice,nextVal, asset)
 
 #Binary Tree 54
 asset = ['Character/prince2.png', 'Others/tanaman obat.png']
-text = ['Wah, aku mendapatkan digit kedua..tetapi aku terluka cukup parah, aku harus segera mengobatinya.', 'tanaman obat yang mana ya yang benar?']
+text = ['Wah, aku mendapatkan digit kedua..tetapi aku terluka cukup parah, aku harus segera mengobatinya.', 'tanaman obat yang mana ya yang benar?', 'Proses mengeluarkan sebuah elemen', 'dari queue disebut dengan?']
 choice = ['dequeue', 'enqueue']
 nextVal = [52, 56]
 dialogueTree.insert(54,text,choice,nextVal, asset)
 
 #Binary Tree 42
 asset = ['Background/black panther.jpg','Character/prince2.png']
-text = ['']
+text = ['Jenis linked list yang memiliki 1 pointer adalah?']
 choice = ['Single', 'Linear']
 nextVal = [38,  46]
 dialogueTree.insert(42,text,choice,nextVal, asset)
@@ -343,7 +343,7 @@ dialogueTree.insert(56,text,choice,nextVal, asset)
 
 #Binary Tree 62
 asset = ['Character/prince2.png']
-text = ['']
+text = [' Salah satu metode untuk mengatasi collision dengan menggunakan pointer']
 choice = ['LISCH', 'ELICH']
 nextVal = [60, 64]
 dialogueTree.insert(62,text,choice,nextVal, asset)
@@ -378,28 +378,28 @@ dialogueTree.insert(64,text,choice,nextVal, asset)
 
 #Binary Tree 98
 asset = ['Background/hutan siang.jpg','Character/prince2.png', 'ds-icon.png']
-text = ['Lihat… aku berhasil mendapatkan 1 digit.', 'Selamat, kamu hanya perlu mencari 1 digit lagi.', 'Aku tidak sabar untuk segera pulang', 'mari kita lanjutkan perjalanan ini.']
+text = ['Lihat… aku berhasil mendapatkan 1 digit.', 'Selamat, kamu hanya perlu mencari 1 digit lagi.', 'Aku tidak sabar untuk segera pulang', 'mari kita lanjutkan perjalanan ini.', 'Integer merupakan tipe data apa?']
 choice = ['Tunggal', 'Majemuk']
 nextVal = [82, 114]
 dialogueTree.insert(98,text,choice,nextVal, asset)
 
 #Binary Tree 82
 asset = ['Background/rumah kosong.jpg', 'Character/prince2.png']
-text = ['Rumah ini sepertinya tidak berpenghuni,', 'aku akan beristirahat di sini malam ini.']
+text = ['Rumah ini sepertinya tidak berpenghuni,', 'aku akan beristirahat di sini malam ini.', 'Penghubung data-data pada graph disebut dengan?']
 choice = ['Edge', 'Node']
 nextVal = [74, 90]
 dialogueTree.insert(82,text,choice,nextVal, asset)
 
 #Binary Tree 74
 asset = ['Background/jurang sungai.jpg', 'Character/prince2.png']
-text = ['']
+text = ['Apakah binary tree boleh memiliki cabang lebih dari 2?']
 choice = ['Ya', 'Tidak']
 nextVal = [70, 78]
 dialogueTree.insert(74,text,choice,nextVal, asset)
 
 #Binary Tree 70
 asset = ['Background/jurang sungai.jpg', 'Character/prince2.png']
-text = ['Loh kok aku tidak mendapatkan digit lagi? Bagaimana aku bisa pulang?', 'Kamu harus mencari jalan lain selain lewat gerbang', 'Hah? Bagaimana caranya?', 'Sudah liat saja nanti, kita temukan dulu kerajaanmu']
+text = ['Loh kok aku tidak mendapatkan digit lagi? Bagaimana aku bisa pulang?', 'Kamu harus mencari jalan lain selain lewat gerbang', 'Hah? Bagaimana caranya?', 'Sudah liat saja nanti, kita temukan dulu kerajaanmu', '   Fungsi untuk menambahkan sebuah elemen ke dalam stack disebut dengan?']
 choice = ['Push', 'Pop']
 nextVal = [68,72]
 dialogueTree.insert(70,text,choice,nextVal, asset)
@@ -425,15 +425,15 @@ nextVal = [0, 0]
 dialogueTree.insert(78,text,choice,nextVal, asset)
 
 #Binary Tree 90
-asset = ['Background/rumah kosong.jpg', 'Character/prince2.png']
-text = ['Rumah ini sepertinya tidak berpenghuni,', 'aku akan beristirahat di sini malam ini.']
+asset = ['Background/jurang sungai.jpg', 'Character/prince2.png']
+text = ['Karena tidak melihat ada jembatan untuk diseberangi,', 'Pangeran memutuskan untuk menyeberang dengan cara terjun ke sungai di bawah jurang', 'Setelah berenang ke tepi..', 'Jika operator berada di depan operands, maka notasi tersebut disebut dengan?']
 choice = ['Prefix', 'Postfix']
 nextVal = [86, 94]
 dialogueTree.insert(90,text,choice,nextVal, asset)
 
 #Binary Tree 86
 asset = ['Background/sungai.jpg', 'Character/prince2.png']
-text = ['Pas sekali di situ ada kayu dan korek api! Aku akan menyalakan api untuk menghangatkan tubuh.']
+text = ['Pas sekali di situ ada kayu dan korek api! Aku akan menyalakan api untuk menghangatkan tubuh.', 'Pada AVL Tree, tree dikatakan seimbang bila,' ,'selisih jumlah node kiri dan kanan tidak lebih dari?']
 choice = ['1', '2']
 nextVal = [84, 88]
 dialogueTree.insert(86,text,choice,nextVal, asset)
@@ -447,7 +447,7 @@ dialogueTree.insert(84,text,choice,nextVal, asset)
 
 #Binary Tree 88
 asset = ['Background/castle.jpg', 'Character/prince2.png']
-text = ['Waduh, gawat! Aku hanya punya 1 digit, tidak bisa membuka gerbang ini!', 'Kamu harus mencari jalan lain selain lewat gerbang', 'Hah? Bagaimana caranya?']
+text = ['Waduh, gawat! Aku hanya punya 1 digit, tidak bisa membuka gerbang ini!', 'Kamu harus mencari jalan lain selain lewat gerbang', 'Hah? Bagaimana caranya?', 'Di antara array dan linked list,', 'manakah yang lebih fleksibel?']
 choice = ['Linked List', 'Array']
 nextVal = [87, 89]
 dialogueTree.insert(88,text,choice,nextVal, asset)
@@ -475,7 +475,7 @@ dialogueTree.insert(94,text,choice,nextVal, asset)
 
 #Binary Tree 114
 asset = ['Background/hutan malam.jpg','Character/prince2.png']
-text = ['Malang sekali nasibku! Tidak menemukan makanan, kini mau beristirahat pun tidak ada tempat berteduh.', '(menghela nafas) hm..aku akan tidur di bawah pohon saja malam ini.', 'Gawat mereka menemukan keberadaan ku. Aku harus segera lari.']
+text = ['Malang sekali nasibku! Tidak menemukan makanan, kini mau beristirahat pun tidak ada tempat berteduh.', '(menghela nafas) hm..aku akan tidur di bawah pohon saja malam ini.', 'Gawat mereka menemukan keberadaan ku. Aku harus segera lari.', 'Apakah binary tree boleh memiliki cabang lebih dari 2?']
 choice = ['Tidak', 'Ya']
 nextVal = [106, 122]
 dialogueTree.insert(114,text,choice,nextVal, asset)
@@ -489,7 +489,7 @@ dialogueTree.insert(106,text,choice,nextVal, asset)
 
 #Binary Tree 102
 asset = ['Background/sungai.jpg','Character/prince2.png']
-text = ['Astaga betapa beruntungnya aku menemukan kayu dan korek api ditempat ini, aku harus segera menghangatkan diri.']
+text = ['Astaga betapa beruntungnya aku menemukan kayu dan korek api ditempat ini, aku harus segera menghangatkan diri.', 'Pada AVL Tree, tree dikatakan seimbang bila,', 'selisih jumlah node kiri dan kanan tidak lebih dari?']
 choice = ['1', '2']
 nextVal = [100,104]
 dialogueTree.insert(102,text,choice,nextVal, asset)
@@ -510,7 +510,7 @@ dialogueTree.insert(100,text,choice,nextVal, asset)
 
 #Binary Tree 104
 asset = ['Background/castle.jpg','Character/prince2.png']
-text = ['Waduh, gawat! Aku hanya punya 1 digit, tidak bisa membuka gerbang ini!', 'Kamu harus mencari jalan lain selain lewat gerbang', 'Hah? Bagaimana caranya?']
+text = ['Waduh, gawat! Aku hanya punya 1 digit, tidak bisa membuka gerbang ini!', 'Kamu harus mencari jalan lain selain lewat gerbang', 'Hah? Bagaimana caranya?', 'Di antara array dan linked list,', 'manakah yang lebih fleksibel?']
 choice = ['Linked List', 'Array']
 nextVal = [57,59]
 dialogueTree.insert(104,text,choice,nextVal, asset)
@@ -552,7 +552,7 @@ dialogueTree.insert(162,text,choice,nextVal, asset)
 
 #Binary Tree 226
 asset = ['Background/hutan pagi.jpg', 'Character/prince2.png']
-text = ['Gawat ada orang yang mau menculikku di sana, sebelum mereka sadar akan keberadaan ku aku harus segera bersembunyi.']
+text = ['Gawat ada orang yang mau menculikku di sana, sebelum mereka sadar akan keberadaan ku aku harus segera bersembunyi.', 'String merupakan tipe data apa?']
 choice = ['Majemuk', 'Tunggal']
 nextVal = [210,  242]
 dialogueTree.insert(226,text,choice,nextVal, asset)
@@ -566,7 +566,7 @@ dialogueTree.insert(210,text,choice,nextVal, asset)
 
 #Binary Tree 242
 asset = ['Background/semak-semak.jpg', 'Character/prince2.png','Character/villain1.png']
-text = ['Lepaskan aku, jangan ikat aku di pohon, pohon ini banyak semut nya.', 'Sudah diam saja, jangan banyak bicara. Awas saja kalau kau kabur lagi.']
+text = ['Lepaskan aku, jangan ikat aku di pohon, pohon ini banyak semut nya.', 'Sudah diam saja, jangan banyak bicara. Awas saja kalau kau kabur lagi.', ' Array merupakan struktur data apa?']
 choice = ['Sederhana', 'Majemuk']
 nextVal = [234,  250]
 dialogueTree.insert(242,text,choice,nextVal, asset)
